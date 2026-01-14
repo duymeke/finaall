b396b50905eeb5763ce655e1b58f7b4c5d120da7
Dinmukhamed <suleimenovdimash7@gmail.com>
2026-01-14 19:05:57 +0500

    Mapper added

---
+++ src/main/java/com/duymeke/finaall/mapper/WorkItemMapper.java
@@ -0,0 +1,21 @@
+package com.duymeke.finaall.mapper;
+
+import com.duymeke.finaall.dto.WorkItemDto;
+import com.duymeke.finaall.entity.WorkItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {LabelMapper.class})
+public interface WorkItemMapper {
+
+    @Mapping(source = "account.id", target = "accountId")
+    WorkItemDto toDto(WorkItem workItem);
+
+    @Mapping(target = "account", ignore = true)
+    @Mapping(target = "labels", ignore = true)
+    WorkItem toEntity(WorkItemDto dto);
+
+    List<WorkItemDto> toDtoList(List<WorkItem> workItems);
+}
