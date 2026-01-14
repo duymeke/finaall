4700c00cdc3bc7d183633f62e2d8313975cf0faa
Dinmukhamed <suleimenovdimash7@gmail.com>
2026-01-14 15:12:06 +0500

    first commit

---
+++ src/main/java/com/duymeke/finaall/FinaallApplication.java
@@ -0,0 +1,13 @@
+package com.duymeke.finaall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaallApplication.class, args);
+	}
+
+}
